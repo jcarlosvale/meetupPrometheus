d8fedca1037cf4f7272b981b4d9c0f85bf1baacc
Joao Vale <jcarlos.vale@gmail.com>
2022-07-25 22:36:11 +0100

    initial push

---
+++ src/main/java/learning/prometheus/controller/SimpleController.java
@@ -0,0 +1,41 @@
+package learning.prometheus.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping(path={"/simple/{status}", "/simple/"})
+    public ResponseEntity<String> getEndpoint(@PathVariable(name="status", required = false) final Integer statusCode) {
+
+        return createResponse(statusCode);
+    }
+
+    @PostMapping(path={"/simple/{status}", "/simple/"})
+    public ResponseEntity<String> postEndpoint(@PathVariable(name="status", required = false) final Integer statusCode) {
+
+        return createResponse(statusCode);
+    }
+
+    @DeleteMapping(path={"/simple/{status}", "/simple/"})
+    public ResponseEntity<String> deleteEndpoint(@PathVariable(name="status", required = false) final Integer statusCode) {
+
+        return createResponse(statusCode);
+    }
+
+    @PutMapping(path={"/simple/{status}", "/simple/"})
+    public ResponseEntity<String> putEndpoint(@PathVariable(name="status", required = false) final Integer statusCode) {
+
+        return createResponse(statusCode);
+    }
+
+    private ResponseEntity<String> createResponse(Integer statusCode) {
+        if (null != statusCode) {
+            return ResponseEntity.status(statusCode).body("RESPONSE");
+        }
+
+        return ResponseEntity.ok("RESPONSE");
+    }
+}
